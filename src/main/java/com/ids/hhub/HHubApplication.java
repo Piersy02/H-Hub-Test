a1ec8a19704138c12b408f09fa13e3524d8fbb3b
Piersy02 <saveriom.piersigilli@studenti.unicam.it>
2025-12-10 17:30:52 +0100

    Setup iniziale del progetto Spring Boot con dipendenze, Aggiunti Spring Web, JPA, Lombok e H2.

---
+++ src/main/java/com/ids/hhub/HHubApplication.java
@@ -0,0 +1,13 @@
+package com.ids.hhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ids/hhub/HHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ids.hhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
